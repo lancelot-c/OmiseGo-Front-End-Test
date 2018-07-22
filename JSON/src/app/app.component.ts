a7fa5e53cca6969e5f8e568f95e33540a45a0792
Lancelot <lancelot.chardonnet@gmail.com>
2018-07-22 13:07:12 -0600

    Added logic
    The processing turning the input JSON into the output JSON

--- JSON/src/app/app.component.ts
+++ JSON/src/app/app.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  inputText: string
+  outputText: string
+
+  constructor() {
+  }
+
+
+  updateOutput() {
+    let inputJSON
+
+    try {
+      inputJSON = JSON.parse(this.inputText)
+    } catch(e) {
+      this.outputText = 'Invalid JSON'
+      return;
+    }
+
+    let keys = Object.keys(inputJSON)
+    let outputJSON
+
+    for (let i = 0; i < keys.length; i++) {
+      for (let j = 0; j < inputJSON[keys[i]].length; j++) {
+        outputJSON = this.addItem(inputJSON[keys[i]][j], outputJSON)
+      }
+    }
+
+    this.outputText = JSON.stringify(outputJSON, undefined, 2)
+  }
+
+
+  addItem(item, json) {
+    if (json == undefined)
+      return [item]
+
+    for (let i = 0; i < json.length; i++) {
+
+      if (json[i].id == item.parent_id)
+        json[i].children.push(item)
+
+      if (json[i].children.length > 0)
+        json[i].children = this.addItem(item, json[i].children)
+    }
+
+    return json
+  }
+
 }
